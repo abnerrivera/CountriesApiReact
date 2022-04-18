0feee112845dcfc5b90aced3fd16baf142e44703
abnerrivera <abner500edrey@gmail.com>
2022-04-18 15:54:53 -0500

    prueba

--- src/componentes/BuscadorFiltro.js
+++ src/componentes/BuscadorFiltro.js
@@ -8,7 +8,7 @@ const BuscadorFiltro = ({ setRegion, setSearch }) => {
 
             {/*BUSCADOR (onChange: ejecuta una funcion al hacer cambios en el componente)*/}
             {/*onChange={(nombre que damos a todo el objeto en este caso el input) => setSearch(tomamos el valor de lo digitado y se lo asignamos a setSearch)}*/}
-            <input className='buscador' type="text" placeholder='Search for a Country...' onChange={(buscador) => setSearch(buscador.target.value)} />
+            <input className='buscador' type="text" placeholder='-Search for a Country...' onChange={(buscador) => setSearch(buscador.target.value)} />
 
             {/*onChange={(nombre que le asiganamos al objeto) => setRegion(toma el valor actual del select y lo asigna a setRegion)}*/}
             <select onChange={(selector) => setRegion(selector.target.value)} className="selector" name="filtro">
